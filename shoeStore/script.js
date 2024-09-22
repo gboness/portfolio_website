d9d805a3f07f4ebaeeba5402d81d33fe1f9bc636
gboness <tylergaucher10@gmail.com>
2024-09-22 10:53:27 -0400

    next and back btn
    fixed next and back buttons to work

--- shoeStore/script.js
+++ shoeStore/script.js
@@ -1,40 +1,14 @@
 
-//change photo function
+// change photo function
 
-// function changeImage() {
-//     var img = document.getElementById("image");
-//     if()
-//     img.src="/asset/furia-air.jpg";   
-//         img.src="/asset/drago-lv.jpg";
-// }
+function changeImage() {
+    var img = document.getElementById("image");
+    img.src="/asset/drago-lv.jpg";
+    return false;
+}
 
-// function changeImageBack() {
-//     var img = document.getElementById("image");
-//     img.src="/asset/furia-air.jpg";
-//     return false;
-// }
-
-const images = [
-    './asset/drago-lv.jpg', 
-    './asset/furia-air.jpg',
-    './asset/instinct-vsr.jpg'
-];
-
-let currentIndex = 0;
-
-const imageElement = document.getElementById('imageselector');
-const switchButton = document.getElementById('clickme');
-
-// Add a click event listener to the button
-switchButton.addEventListener('click', () => {
-  // Increment the index to switch to the next image
-  currentIndex++;
-
-// If the current index exceeds the number of images, reset it to 0 (loop back)
-    if (currentIndex >= images.length) {
-        currentIndex = 0;
-      }
-    
-      // Change the image source to the new image
-      imageElement.src = images[currentIndex];
-    });
+function changeImageBack() {
+    var img = document.getElementById("image");
+    img.src="/asset/furia-air.jpg";
+    return false;
+}
